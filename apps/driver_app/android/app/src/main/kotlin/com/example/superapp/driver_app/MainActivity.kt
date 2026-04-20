595a3e171a3b716cd34675fceaed2782e24a45a6
abdulbosit <abdulbositabduvosiqov32@gmail.com>
2026-04-20 15:58:58 +0500

    Some changes

---
+++ apps/driver_app/android/app/src/main/kotlin/com/example/superapp/driver_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.superapp.driver_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/passenger_app/android/app/src/main/kotlin/com/example/superapp/passenger_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.superapp.passenger_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
